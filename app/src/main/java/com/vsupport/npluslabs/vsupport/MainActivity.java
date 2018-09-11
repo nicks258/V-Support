b1b13bd1798ab0837ab0dee5547d8b104329903f
sumit <mehrasumit258@gmail.com>
2018-09-12 01:46:30 +0400

    intial commit..

---
+++ app/src/main/java/com/vsupport/npluslabs/vsupport/MainActivity.java
@@ -0,0 +1,145 @@
+package com.vsupport.npluslabs.vsupport;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements ExpandableListView.OnChildClickListener {
+
+    Toolbar toolbar;
+    private DrawerLayout drawer;
+    private ExpandableListView drawerList;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        initDrawer();
+    }
+
+    private void initDrawer() {
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        drawerList = (ExpandableListView) findViewById(R.id.left_drawer);
+
+        // preparing list data
+        prepareListData();
+
+        drawerList.setAdapter(new NavAdapter(this, listDataHeader, listDataChild));
+
+        drawerList.setOnChildClickListener(this);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawer,toolbar,R.string.drawer_open , R.string.drawer_close ){
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we don't want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we don't want anything to happen so we leave this blank
+
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        //Setting the actionbarToggle to drawer layout
+        drawer.setDrawerListener(actionBarDrawerToggle);
+
+        //calling sync state is necessay or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
+    }
+
+    /*
+     * Preparing the list data
+     */
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+
+        // Adding headers
+        Resources res = getResources();
+        String[] headers = res.getStringArray(R.array.nav_drawer_labels);
+        listDataHeader = Arrays.asList(headers);
+
+        //Adding child data
+
+//        //Dynamic method
+//        for (int i =0; i<listDataHeader.size(); i++){
+//
+//            //Save data in array
+//            String[] childData = res.getStringArray(R.array.elements_home);
+//
+//            //Put data in List
+//            List<String> listChild;
+//            listChild = Arrays.asList(childData);
+//
+//            //Add to hashMap
+//            listDataChild.put(listDataHeader.get(i),listChild);
+//        }
+
+        // Static method
+        List<String> home,friends,notifs;
+        String[] shome,sfriends,snotifs;
+
+        shome = res.getStringArray(R.array.elements_home);
+        home = Arrays.asList(shome);
+
+        sfriends = res.getStringArray(R.array.elements_friends);
+        friends = Arrays.asList(sfriends);
+
+        snotifs = res.getStringArray(R.array.elements_notifs);
+        notifs = Arrays.asList(snotifs);
+
+        // Add to hashMap
+        listDataChild.put(listDataHeader.get(0), home); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), friends);
+        listDataChild.put(listDataHeader.get(2), notifs);
+    }
+
+    @Override
+    public boolean onChildClick(ExpandableListView parent, View v,
+                                int groupPosition, int childPosition, long id) {
+        Toast.makeText(
+                getApplicationContext(),
+                listDataHeader.get(groupPosition)
+                        + " : "
+                        + listDataChild.get(
+                        listDataHeader.get(groupPosition)).get(
+                        childPosition), Toast.LENGTH_SHORT)
+                .show();
+        if(listDataChild.get(
+                listDataHeader.get(groupPosition)).get(
+                childPosition).equals("Telugu")){
+            TamilShows tamilShows = new TamilShows();
+            getSupportFragmentManager().beginTransaction().replace(R.id.container_body,tamilShows).commit();
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
